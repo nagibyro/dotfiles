e4d322bc4d05206609913f0de83e4acb736a698a
Byron Nagi <byro@fastmail.fm>
2021-12-17 22:48:51 -0500

    add battery to sway status bar

--- linux/arch/wayland/swaybar-status-cmd.sh
+++ linux/arch/wayland/swaybar-status-cmd.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 set -eo pipefail
 
-while date +'%Y-%m-%d %l:%M:%S %p'; do sleep 1; done
+date_formatted=$(date +'%Y-%m-%d %l:%M:%S %p')
+
+battery_percentage=$(sudo tlp-stat -b | grep Charge | awk '{ print $3 }')
+battery_status=$(sudo tlp-stat -b | grep status | awk '{ print $3 }')
+
+echo "$date_formatted | $battery_percentage% - $battery_status"
