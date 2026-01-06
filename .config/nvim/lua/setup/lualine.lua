49921c9bc554f0ab0bee30a123180e294118f833
Byron Nagi <byro@fastmail.fm>
2026-01-06 16:50:19 -0500

    update deps and have lualine filename be relative path

--- .config/nvim/lua/setup/lualine.lua
+++ .config/nvim/lua/setup/lualine.lua
@@ -1,22 +1,22 @@
 require("lualine").setup({
-  sections = {
-    lualine_a = { "mode" },
-    lualine_b = { "branch", "diff", "diagnostics" },
-    lualine_c = { "filename" },
-    lualine_x = { "encoding", "filetype" },
-    lualine_y = {
-      {
-        "datetime",
-        style = "%A, %B %d | %I:%M %p",
-      },
-    },
-    lualine_z = { "location" },
-  },
-  -- options = {
-  --   icons_enabled = false,
-  --   -- theme = false,
-  --   compo = false,
-  --   icons_enabled = false,
+	sections = {
+		lualine_a = { "mode" },
+		lualine_b = { "branch", "diff", "diagnostics" },
+		lualine_c = { { "filename", path = 1 } },
+		lualine_x = { "encoding", "filetype" },
+		lualine_y = {
+			{
+				"datetime",
+				style = "%A, %B %d | %I:%M %p",
+			},
+		},
+		lualine_z = { "location" },
+	},
+	-- options = {
+	--   icons_enabled = false,
+	--   -- theme = false,
+	--   compo = false,
+	--   icons_enabled = false,
 
-  -- }
+	-- }
 })
